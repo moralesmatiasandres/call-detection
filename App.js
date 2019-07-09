0a6e428224284800a51e8abcbdfb41e24ed69ea2
MAtias Morales <moralesmatiasandres@gmail.com>
2019-07-08 21:25:59 -0300

    call config

--- App.js
+++ App.js
@@ -9,21 +9,69 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import CallDetectorManager from 'react-native-call-detection'
 
 type Props = {};
 export default class App extends Component<Props> {
+  componentDidMount() {
+	this.callDetector = new CallDetectorManager((event)=> {
+	// For iOS event will be either "Connected",
+	// "Disconnected","Dialing" and "Incoming"
+	
+	// For Android event will be either "Offhook",
+	// "Disconnected", "Incoming" or "Missed"
+	
+
+	if (event === 'Disconnected') {
+    console.log('termino llamada')
+	// Do something call got disconnected
+	} 
+	else if (event === 'Connected') {
+    console.log('HOLa')
+
+	// Do something call got connected
+	// This clause will only be executed for iOS
+	} 
+	else if (event === 'Incoming') {
+    console.log('HOLa2')
+
+	// Do something call got incoming
+	}
+	else if (event === 'Dialing') {
+    console.log('HOLa3')
+
+	// Do something call got dialing
+	// This clause will only be executed for iOS
+	} 
+	else if (event === 'Offhook') {
+    console.log('HOLa4')
+
+	//Device call state: Off-hook. 
+	// At least one call exists that is dialing,
+	// active, or on hold, 
+	// and no calls are ringing or waiting.
+	// This clause will only be executed for Android
+	}
+	else if (event === 'Missed') {
+    console.log('HOLa5')
+
+    	// Do something call got missed
+    	// This clause will only be executed for Android
+    }
+},
+false, // if you want to read the phone number of the incoming call [ANDROID], otherwise false
+()=>{}, // callback if your permission got denied [ANDROID] [only if you want to read incoming number] default: console.error
+{
+title: 'Phone State Permission',
+message: 'This app needs access to your phone state in order to react and/or to adapt to incoming calls.'
+} // a custom permission request message to explain to your user, why you need the permission [recommended] - this is the default one
+)
+
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
